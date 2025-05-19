cc3c30b140500d27f9f6b4492b58cf49b1f4ce7d
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-05-19 21:15:50 +0530

    feat: Problem controllers template

---
+++ Backend/src/controllers/problem.controller.js
@@ -0,0 +1,17 @@
+const createProblem = async (req, res) => {
+
+};
+
+const getProblem = async (req, res) => {
+
+};
+
+const updateProblem = async (req, res) => {
+
+};
+
+const deleteProblem = async (req, res) => {
+
+};
+
+export { createProblem, getProblem, updateProblem, deleteProblem };
