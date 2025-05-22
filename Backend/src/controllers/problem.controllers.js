91cf3d75448b927737d21a34c0ee72733337ba1a
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-05-22 21:04:23 +0530

    updated

--- Backend/src/controllers/problem.controllers.js
+++ Backend/src/controllers/problem.controllers.js
@@ -1,5 +1,23 @@
-const createProblem = async (req, res) => {
+import { Role } from "../generated/prisma/index.js";
+import db from "../libs/database.js";
+import { ApiError } from "../utils/ApiError.js";
 
+const createProblem = async (req, res) => {
+      const { title, description, difficulty, tags, examples, constraints, hints, editorial, testcases, codeSnippets, referenceSolutions } = req.body;
+
+      if ([title, description, difficulty, tags, examples, constraints, hints, editorial, testcases, codeSnippets, referenceSolutions].some((field) => field?.trim() === "")) {
+            throw new ApiError(400, "All fields are required");
+      };
+
+      if (req.user.role !== Role.ADMIN) {
+            throw new ApiError(403, "You are not authorized to create a problem");
+      };
+
+      try {
+
+      } catch (error) {
+
+      }
 };
 
 const getAllProblems = async (req, res) => {
---
+++ Backend/src/libs/judge0.js
@@ -0,0 +1,9 @@
+export const getJudge0LanguageId = (language) => {
+      const languageMap = {
+            "JAVASCRIPT": 63,
+            "PYTHON": 71,
+            "JAVA": 62,
+            "C++": 66,
+            "C": 67,
+      }
+}
