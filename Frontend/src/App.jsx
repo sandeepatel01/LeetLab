4328839d62f725dd7b4ff3961fb05f01ceb9dc21
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-05-25 21:50:03 +0530

    feat: Authorized route setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,28 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./page/HomePage";
 import LoginPage from "./page/LoginPage";
 import SignupPage from "./page/SignupPage";
+import LandingPage from "./page/LandingPage";
 
 const App = () => {
+  let authUser = null;
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<LandingPage />} />
+
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+        />
       </Routes>
     </div>
   );
