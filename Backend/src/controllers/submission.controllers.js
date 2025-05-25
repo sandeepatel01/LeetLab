6cc552417c6cea86ce2a413cca88a30ca4b0fdb6
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-05-25 09:17:03 +0530

    feat: getAllSubmission

--- Backend/src/controllers/submission.controllers.js
+++ Backend/src/controllers/submission.controllers.js
@@ -1,6 +1,31 @@
+import { db } from "../libs/database.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getAllSubmission = async (req, res) => {
+      try {
+            const userId = req.user.id;
 
+            if (!userId) {
+                  throw new ApiError(400, "User not found");
+            }
+
+            const submissions = await db.submission.findMany({
+                  where: {
+                        userId: userId
+                  }
+            });
+
+            res.status(200).json(
+                  new ApiResponse(
+                        200,
+                        "Submissions fetched successfully",
+                        submissions
+                  )
+            )
+      } catch (error) {
+            throw new ApiError(500, error?.message || "Error fetching submissions");
+      }
 };
 
 const getSubmissionsForProblem = async (req, res) => {
