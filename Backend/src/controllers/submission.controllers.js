d9f4d9606de51f601f14bb82790c68e484cd1177
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-05-25 09:23:55 +0530

    feat: getAllTheSubmissionsForProblem

--- Backend/src/controllers/submission.controllers.js
+++ Backend/src/controllers/submission.controllers.js
@@ -36,7 +36,7 @@ const getSubmissionsForProblem = async (req, res) => {
                   throw new ApiError(400, "User not found");
             };
 
-            const problemId = req.params.id;
+            const problemId = req.params.problemId;
 
             if (!problemId) {
                   throw new ApiError(400, "Problem not found");
@@ -62,11 +62,34 @@ const getSubmissionsForProblem = async (req, res) => {
 };
 
 const getAllTheSubmissionsForProblem = async (req, res) => {
+      try {
+            const problemId = req.params.problemId;
 
+            if (!problemId) {
+                  throw new ApiError(400, "Problem not found");
+            };
+
+            const submissions = await db.submission.count({
+                  where: {
+                        problemId: problemId
+                  }
+            });
+
+            res.status(200).json(
+                  new ApiResponse(
+                        200,
+                        "Submissions fetched successfully",
+                        submissions
+                  )
+            )
+      } catch (error) {
+            throw new ApiError(500, error?.message || "Error fetching submissions");
+      }
 };
+
 
 export {
       getAllSubmission,
       getSubmissionsForProblem,
       getAllTheSubmissionsForProblem
-}
+};
