609cb826e1732cc19fe6205768d47cc6b28a1de9
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-05-27 13:46:32 +0530

    feat: login page ready

--- Frontend/src/lib/validation.js
+++ Frontend/src/lib/validation.js
@@ -4,6 +4,11 @@ export const signupSchema = z.object({
       name: z.string(3, "Name must be at least 3 characters long"),
       username: z.string(2, "Username must be at least 2 characters long"),
       email: z.string().email("Enter a valid email"),
-      password: z.string().min(6, "Password must be at least 6 characters long"),
+      password: z.string().min(8, "Password must be at least 8 characters long"),
       avatar: z.string().url("Enter a valid url"),
-})
+});
+
+export const LoginSchema = z.object({
+      email: z.string().email("Enter a valid email"),
+      password: z.string().min(8, "Password must be at least 8 characters long"),
+});
